34cfec2d3df7cf8cb025b3e31cb1994c1fa992e4
yj <bit-user@bit>
2016-09-06 17:17:24 +0900

    17:17

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -1,9 +1,14 @@
 package practice01;
 
-
 public class Prob2 {
 
 	public static void main(String[] args) {
+		int countNum = 1;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 10; j++){
+				System.out.printf(countNum+i+j + " ");
+			}
+			System.out.println("");
+		}
 	}
-
 }
