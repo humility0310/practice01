676fbfcd117259f935513efdf814bb92fca8781b
yj <bit-user@bit>
2016-09-06 17:13:08 +0900

    17:13

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -1,4 +1,7 @@
 package practice01;
+
+import java.util.Scanner;
+
 public class Game369 {
 	public static void main(String[] args) {
 	}
--- src/practice01/GuessNumber.java
+++ src/practice01/GuessNumber.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 public class GuessNumber {
 	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner( System.in );
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.printf("수를 입력하세요 :");
+		int inputNum = scanner.nextInt();
+
+		if (inputNum % 3 == 0) {
+			System.out.println("3의 배수입니다.");
+		} else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
 		scanner.close();
 	}
 }
